c4e6eb18a1ce73955f98feaef2c28d0475a67aa4
brunocoutinhoo <brunocoutinho64@gmail.com>
2022-05-12 22:39:17 -0300

    scroll pagina de detalhes toda

--- flutter_project/lib/detailpage.dart
+++ flutter_project/lib/detailpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/barraca.dart';
+import 'package:flutter_project/quentinha.dart';
 import 'package:flutter_project/reviewpage.dart';
 import 'package:flutter_project/style.dart';
 import 'package:flutter_project/palette.dart';
@@ -15,65 +16,84 @@ class DetailPage extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: Text(barraca.nomeBarraca)),
         body: Container(
-            color: Color.fromARGB(218, 250, 227, 227),
-            constraints: BoxConstraints.expand(),
-            child: Center(
-                child: Column(children: <Widget>[
-              const SizedBox(
-                height: 30,
-              ),
-              if (barraca.imagemBarraca != "IMAGEM")
-                GestureDetector(
-                    onDoubleTap: () {
-                      ImageDialog(barraca.imagemBarraca);
-                    },
-                    child: (Image.asset(
-                      barraca.imagemBarraca,
-                      width: 250.0,
-                      height: 250.0,
-                    ))),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                  margin: EdgeInsets.all(20), child: Text(barraca.descricao)),
-              Expanded(
-                  child: ListView.builder(
-                      itemCount: barraca.quentinhas.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                          margin: EdgeInsets.all(20),
-                          height: 100,
-                          width: double.infinity,
-                          child: Card(
-                              color: createMaterialColor(
-                                  Color.fromARGB(255, 253, 202, 168)),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  side: BorderSide(
-                                    color: Colors.grey.withOpacity(0.2),
-                                    width: 1,
-                                  )),
-                              child: GestureDetector(
-                                  onTap: () {
-                                    Navigator.pushNamed(
-                                        context, ReviewPage.routeName,
-                                        arguments: barraca);
-                                  },
-                                  child: SizedBox(
-                                      width: 100,
-                                      height: 100,
-                                      child: Stack(children: [
-                                        Container(
-                                            child: Center(
-                                          child: Text(
-                                              barraca.quentinhas[index].nome,
-                                              style: Style.headerTextStyle),
-                                        )),
-                                      ])))),
-                        );
-                      }))
-            ]))));
+            color: const Color.fromARGB(218, 250, 227, 227),
+            constraints: const BoxConstraints.expand(),
+            child: ListView(
+              children: <Widget>[
+                if (barraca.imagemBarraca != "IMAGEM")
+                  exibeImagemBarraca(barraca),
+                WidgetResumoBarraca(barraca: barraca),
+                for (var quentinha in barraca.quentinhas)
+                  WidgetQuentinha(quentinha: quentinha)
+              ],
+            )));
+  }
+
+  GestureDetector exibeImagemBarraca(Barraca barraca) {
+    return GestureDetector(
+        onDoubleTap: () {
+          ImageDialog(barraca.imagemBarraca);
+        },
+        child: (Image.asset(
+          barraca.imagemBarraca,
+          width: 250.0,
+          height: 250.0,
+        )));
+  }
+}
+
+class WidgetResumoBarraca extends StatelessWidget {
+  const WidgetResumoBarraca({
+    Key? key,
+    required this.barraca,
+  }) : super(key: key);
+
+  final Barraca barraca;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, ReviewPage.routeName,
+              arguments: barraca);
+        },
+        child: Container(
+            margin: EdgeInsets.all(20), child: Text(barraca.descricao)));
+  }
+}
+
+class WidgetQuentinha extends StatelessWidget {
+  const WidgetQuentinha({
+    Key? key,
+    required this.quentinha,
+  }) : super(key: key);
+
+  final Quentinha quentinha;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      height: 100,
+      width: double.infinity,
+      child: Card(
+          color: createMaterialColor(Color.fromARGB(255, 253, 202, 168)),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+              side: BorderSide(
+                color: Colors.grey.withOpacity(0.2),
+                width: 1,
+              )),
+          child: SizedBox(
+              width: 100,
+              height: 100,
+              child: Stack(children: [
+                Container(
+                    child: Center(
+                  child: Text(quentinha.nome, style: Style.headerTextStyle),
+                )),
+              ]))),
+    );
   }
 }
 
