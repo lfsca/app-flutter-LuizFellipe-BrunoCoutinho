51e1f5d65686fccebeac0aca3cad296a05b47040
Luiz Fellipe Augusto <lfsca08@gmail.com>
2022-06-08 19:10:01 -0300

    Adiciona usuario por formulario

--- flutter_project/lib/cadastro.dart
+++ flutter_project/lib/cadastro.dart
@@ -16,11 +16,31 @@ class RegisterForm extends StatefulWidget {
 class _RegisterFormState extends State<RegisterForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  final registernomeController = TextEditingController();
+  final registersenhaController = TextEditingController();
+  final registeremailController = TextEditingController();
+  final registervendedorController = TextEditingController();
+
+  String nome = "";
+  String senha = "";
+  String email = "";
+  bool vendedor = false;
+
   @override
   Widget build(BuildContext context) {
     CollectionReference usuario =
         FirebaseFirestore.instance.collection('usuario');
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hora do Rango PUC-Rio'),
+        leading: GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, RegisterForm.routeName);
+            },
+            child: Icon(
+              Icons.menu,
+            )),
+      ),
       body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -30,25 +50,55 @@ class _RegisterFormState extends State<RegisterForm> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   TextFormField(
+                    controller: registernomeController,
+                    decoration: const InputDecoration(
+                      hintText: 'Insira seu nome completo',
+                    ),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor insira seu nome';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: registeremailController,
                     decoration: const InputDecoration(
                       hintText: 'Insira seu email',
                     ),
                     validator: (String? value) {
                       if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
+                        return 'Por favor insira seu email';
                       }
                       return null;
                     },
                   ),
+                  TextFormField(
+                    controller: registersenhaController,
+                    decoration: const InputDecoration(
+                      hintText: 'Insira uma senha',
+                    ),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor insira uma senha';
+                      }
+                      return null;
+                    },
+                  ),
+                  CheckboxWidget(),
+                  Text("E vendedor?"),
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 16.0),
                     child: ElevatedButton(
                       onPressed: () {
                         // Validate will return true if the form is valid, or false if
                         // the form is invalid.
+                        nome = registernomeController.text;
+                        email = registeremailController.text;
+                        senha = registersenhaController.text;
+                        vendedor = false;
                         if (_formKey.currentState!.validate()) {
-                          addUser(usuario, "Luiz", "emailteste", "batatinha",
-                              false);
+                          addUser(usuario, nome, email, senha, vendedor);
                         }
                       },
                       child: const Text('Criar'),
@@ -76,22 +126,39 @@ class _RegisterFormState extends State<RegisterForm> {
   }
 }
 
-// class RegisterData extends StatelessWidget {
-//   final String nome;
-//   final String email;
-//   final String senha;
-//   final bool vendedor;
+class CheckboxWidget extends StatefulWidget {
+  const CheckboxWidget({Key? key}) : super(key: key);
 
-//   RegisterData(this.nome, this.email, this.senha, this.vendedor);
+  @override
+  State<CheckboxWidget> createState() => _CheckboxWidgetState();
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     // Create a CollectionReference called users that references the firestore collection
-//     CollectionReference usuario =
-//         FirebaseFirestore.instance.collection('usuario');
+class _CheckboxWidgetState extends State<CheckboxWidget> {
+  bool isChecked = false;
 
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.white;
+      }
+      return Colors.black;
+    }
 
-
-//     return addUser(),
-//   }
-// }
+    return Checkbox(
+      checkColor: Colors.white,
+      fillColor: MaterialStateProperty.resolveWith(getColor),
+      value: isChecked,
+      onChanged: (bool? value) {
+        setState(() {
+          isChecked = value!;
+        });
+      },
+    );
+  }
+}
